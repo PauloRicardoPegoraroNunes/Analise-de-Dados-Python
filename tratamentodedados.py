b9f646c5807b4e8dc5ca9ec9549fa83938f4c8d2
Paulo Ricardo Pegoraro Nunes <pauloricardopegoraronunes2@gmail.com>
2019-04-22 15:03:04 -0300

    Update tratamentodedados.py

--- tratamentodedados.py
+++ tratamentodedados.py
@@ -1,5 +1,7 @@
 import pandas as pd
 #pode ser usado qualquer arquivo csv
+#link para o dowload do arquivo usado aqui http://dados.gov.br/dataset/mec-pronatec-eptc/resource/0abee23b-9e79-4893-b284-6882f9d39fdd
+#caso use outro arquivo terá que mudar na variavel df abaixo 
 
 df = pd.read_csv('MAT_ATENDIDAS_RF_EPCT_2011_CSV.csv',
                  sep=';', encoding='cp1252')
