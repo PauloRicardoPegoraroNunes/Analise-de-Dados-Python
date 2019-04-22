887209919dcddb36fd221ab783a82528872ce116
Paulo Ricardo Pegoraro Nunes <pauloricardopegoraronunes2@gmail.com>
2019-04-22 14:55:02 -0300

    Update tratamentodedados.py

--- tratamentodedados.py
+++ tratamentodedados.py
@@ -1,19 +1,20 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+#pode ser usado qualquer arquivo csv
 
 df = pd.read_csv('MAT_ATENDIDAS_RF_EPCT_2011_CSV.csv',
                  sep=';', encoding='cp1252')
 
 # le as primeiras 5 linhas do csv
 x = df.head()
-
 # mostra os tipos de dados
 y = df.dtypes
-
 # seleciona os dados a ser mostrado
 c = df['CODIGO_IDENTIFICACAO_MEC'].value_counts()
+
+
+print(x)
 print(y)
 print(c)
-plt.show()
+
 
 
